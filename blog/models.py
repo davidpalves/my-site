cb823b1535abe61215c4589eb4ff68a3d1e465f2
David Pierre <davidpierrealves21@gmail.com>
2021-10-22 00:39:43 -0300

    Add read time to post information

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 from django.utils.text import slugify
 
 from markdown import markdown
+from readtime import of_markdown
 
 from blog.enums import PostStatusEnum, POST_STATUS_ENUM
 from users.models import User
@@ -13,30 +14,35 @@ from users.models import User
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
-
-    title = models.CharField(max_length=255, null=False, blank=False)
-
     slug = models.SlugField(max_length=150, unique=True)
-
+    title = models.CharField(max_length=255, null=False, blank=False)
     status = models.CharField(
-        choices=POST_STATUS_ENUM, default=PostStatusEnum.DRAFT, blank=False, null=False, max_length=10
+        choices=POST_STATUS_ENUM,
+        default=PostStatusEnum.DRAFT,
+        blank=False,
+        null=False,
+        max_length=10,
     )
-
     text = models.TextField()
+    tags = models.CharField(max_length=255, blank=True)
 
-    created_date = models.DateField(default=timezone.now)
-
+    created_date = models.DateField(auto_now_add=True)
     published_date = models.DateField(blank=True, null=True)
-
-    tags = models.ManyToManyField('Tag', related_name="posts", null=True, blank=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
+        self.tags = ", ".join(
+            {tag.strip() for tag in self.tags.split(",")}
+        )
         super(Post, self).save(*args, **kwargs)
 
     def get_text_as_markdown(self):
         return mark_safe(markdown(self.text, safe_mode="escape"))
 
+    @property
+    def read_time(self):
+        return of_markdown(self.text, wpm=225).minutes
+    
     def publish(self):
         if self.status is not PostStatusEnum.PUBLISHED:
             self.published_date = timezone.now()
@@ -55,8 +61,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class Tag(models.Model):
-    label = models.CharField(max_length=50)
-    
--- blog/serializers.py
+++ blog/serializers.py
@@ -1,20 +1,14 @@
 from rest_framework import serializers
-from blog.models import Post, Tag
-from blog.enums import POST_STATUS_ENUM
-
-
-class TagsSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = ('label',)
-        model = Tag
+from blog.models import Post
+from blog.enums import POST_STATUS_ENUM, PostStatusEnum
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.first_name')
     slug = serializers.SlugField(read_only=True, required=False)
-    status = serializers.ChoiceField(choices=POST_STATUS_ENUM, default=0)
-    published_date = serializers.DateField(required=True)
-    tags = TagsSerializer(many=True)
+    status = serializers.ChoiceField(choices=POST_STATUS_ENUM, default=PostStatusEnum.DRAFT)
+    published_date = serializers.DateField(required=False)
+    tags = serializers.SerializerMethodField()
 
     def get_queryset(self):
         slug = self.kwargs['slug']
@@ -27,14 +21,19 @@ class PostSerializer(serializers.ModelSerializer):
             'author',
             'status',
             'text',
+            'tags',
+            'read_time',
             'published_date',
-            'tags'
         )
         model = Post
         lookup_field = 'slug'
         extra_kwargs = {
             'url': {'lookup_field': 'slug'}
         }
+
+    def get_tags(self, obj):
+        tags = obj.tags.split(", ")
+        return tags
 
 
 class ChangePostStatusSerializer(serializers.ModelSerializer):
