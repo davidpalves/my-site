ee9ee85d46b23d3ea0317a3565265e88b3fd2baf
David Pierre <davidpierrealves21@gmail.com>
2021-10-22 12:02:18 -0300

    Add filter by author

--- blog/filters.py
+++ blog/filters.py
@@ -1,7 +1,6 @@
 from django_filters import rest_framework as filters
 
-from blog.models import Post
-
 
 class PostsFilter(filters.FilterSet):
     tags = filters.CharFilter(lookup_expr="icontains")
+    author = filters.CharFilter(lookup_expr="iexact")
